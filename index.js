03614722b96ed948f71fb696f1de1f1f80459622
stellar-function-e8f8 <kwooten1993@gmail.com>
2021-03-30 01:49:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function destructivelyAppendKitten(name){
   return kittens.push(name);
   return kittens;
 }
+
+function destructivelyPrependKitten(name){
+kittens.unshift(name);
+return kittens;
+}
