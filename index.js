e3f5a42973e7ce5dcf3431eee6abeea62cfdec51
stellar-function-e8f8 <kwooten1993@gmail.com>
2021-03-30 01:48:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,5 @@ var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 // Add your functions and code here
 function destructivelyAppendKitten(name){
   return kittens.push(name);
-}
-
-function destructivelyAppendKitten(name){
-return  kittens.unshift(name);
-
+  return kittens;
 }
